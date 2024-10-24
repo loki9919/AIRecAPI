1923c302d35036177f449f0c6116e79dd2266657
lokmane rouijel <lokmane9919@gmail.com>
2024-10-25 00:50:40 +0100

    Import List from typing

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,9 @@
+from typing import List
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Secure Product API"
-    API_KEYS: list[str]
+    API_KEYS: List[str]
     DATABASE_URL: str
 
     class Config:
