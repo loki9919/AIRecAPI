eb980d65c25c2e64b338ff2d47d82a7fe8427ec2
lokmane rouijel <lokmane9919@gmail.com>
2024-10-26 01:42:44 +0100

    remove the groq

--- app/api/endpoints/products.py
+++ app/api/endpoints/products.py
@@ -18,6 +18,16 @@ async def get_similar_products(
         raise HTTPException(status_code=404, detail="Product not found")
     return {"similar_product_ids": [p.id for p in similar_products]}
 
+@router.get("/similar-products-ai/{product_id}")
+async def get_similar_products_ai(
+    product_id: int,
+    db: Session = Depends(get_db),
+    api_key: str = Depends(get_api_key),
+):
+    similar_product_ids = product_service.get_similar_products_embeddings(db, product_id)
+    return {"similar_product_ids": similar_product_ids}
+
+
 @router.api_route("/api/v1/{path:path}", methods=["*"])
 async def api_key_catch_all(request: Request, api_key: str = Depends(get_api_key)):
     raise HTTPException(status_code=404, detail="Not Found")
--- app/core/config.py
+++ app/core/config.py
@@ -5,8 +5,10 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Secure Product API"
     API_KEYS: List[str]
     DATABASE_URL: str
-    GROQ_API_KEY: str
+
+
     class Config:
         env_file = ".env"
 
+# Create an instance of the Settings class
 settings = Settings()
