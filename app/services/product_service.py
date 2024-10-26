3b61965b39e6fedc0abad4893a5669c675955e3f
lokmane rouijel <lokmane9919@gmail.com>
2024-10-26 03:38:44 +0100

    fixed naming issues

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,8 +1,10 @@
+# app/services/product_service.py
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from app.models.product import Product
 import numpy as np
 from app.embeddings import vector_store
+
 
 def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
@@ -17,12 +19,11 @@ def get_similar_products(db: Session, product_id: int):
     similar_products = db.query(Product).filter(Product.id != product_id).filter(or_(*filters)).all()
     return similar_products
 
-async def get_similar_products_rag(db: Session, product_id: int, top_k: int = 3):
 
+async def get_similar_products_embeddings(db: Session, product_id: int, top_k: int = 3):
     product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
-        return None # Handle product not found
-
+        return None  # Handle product not found
 
     if vector_store:
         similar_docs = vector_store.similarity_search_with_score(
@@ -31,4 +32,4 @@ async def get_similar_products_rag(db: Session, product_id: int, top_k: int = 3)
         similar_product_ids = [doc.metadata["product_id"] for doc, _ in similar_docs]
         return similar_product_ids
     else:
-        return []  # Handle case where vector_store is not initialized (e.g., during testing)
+        return []  # Handle case where vector_store is not initialized
