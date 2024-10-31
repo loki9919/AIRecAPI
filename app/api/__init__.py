f0867723d9492549117f7c23307fd6420fcae673
lokmane rouijel <lokmane9919@gmail.com>
2024-10-31 15:31:14 +0100

    Add Streamlit demo file for project UI

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 from flask import Blueprint
 from .namespaces import api
 from .routes.products import products_ns
 
 api_bp = Blueprint("api", __name__)
 api.add_namespace(products_ns)
->>>>>>> db4d8f8... fix: Migrate from FastAPI to FlaskAPI due to dependency issues
